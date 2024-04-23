a87ab51fbf955ba49d0478ff17627ee5ed8e019a
cereia <104402370+cereia@users.noreply.github.com>
2024-04-23 15:36:38 -0400

    Add condition to prevent overwriting
    Check that the spot is empty before adding a mark

--- script.rb
+++ script.rb
@@ -12,10 +12,22 @@ class Board
   end
 
   def place(mark, position)
-    board[position - 1] = mark
-    p board
+    if board[position - 1] == ''
+      board[position - 1] = mark
+      p board
+    else
+      puts 'There\'s already a symbol there. Please choose again.'
+    end
   end
 end
 
 a = Board.new
 a.place('X', 3)
+a.place('O', 5)
+a.place('X', 5)
+
+{
+  horizontal: [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+  vertical: [[1, 4, 7], [2, 5, 8], [3, 6, 9]],
+  diagonal: [[1, 5, 9], [3, 5, 7]]
+}
