d8f0653321f07ace0ec89a44deab0fabf9c5f987
cereia <104402370+cereia@users.noreply.github.com>
2024-04-24 15:44:33 -0400

    Add round counter to determine player turns
    Also connected player symbol to the board instance for ease of use

--- script.rb
+++ script.rb
@@ -3,21 +3,26 @@
 # create a tic tac toe game where two humans can play
 # the board is meant to be displayed in the console after every player turn
 class Board
-  attr_reader :board
+  attr_reader :board, :round
 
-  def initialize
+  def initialize(hash)
     @board = []
     0.upto(8) { |i| @board[i] = '' }
-    p board
+    @round = 0
+    @player1 = hash[:player1]
+    @player2 = hash[:player2]
+    puts "Player 1: #{@player1} Player 2: #{@player2}"
+    puts "Board created: #{board}"
   end
 
   def place(mark, position)
+    @round += 1
     if board[position - 1] == ''
       board[position - 1] = mark
     else
       puts 'There\'s already a symbol there. Please choose again.'
     end
-    p board
+    puts "Round: #{@round} #{board}"
   end
 end
 
@@ -25,13 +30,13 @@ def play_game
   puts 'Would you like to play tic tac toe? Y/N'
   answer = gets
   if answer[0].match(/y/i)
-    assign_symbol
-    Board.new
+    board = Board.new(assign_symbol)
   elsif answer[0].match(/n/i)
     puts ':( No tic tac toe'
   else
     play_game
   end
+  board
 end
 
 def assign_symbol
@@ -40,8 +45,7 @@ def assign_symbol
   if mark.match(/x/i) || mark.match(/o/i)
     player1 = (mark.match(/x/i) || mark.match(/o/i)).to_s.upcase
     player2 = player1 == 'X' ? 'O' : 'X'
-    marks = { player1: player1, player2: player2 }
-    puts "Here are your players: #{marks}"
+    { player1: player1, player2: player2 }
   else
     assign_symbol
   end
@@ -55,3 +59,8 @@ play_game
   vertical: [[1, 4, 7], [2, 5, 8], [3, 6, 9]],
   diagonal: [[1, 5, 9], [3, 5, 7]]
 }
+
+# a = Board.new
+# a.place('X', 4)
+# a.place('O', 5)
+# puts a.round
