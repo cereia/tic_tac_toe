8ac6dc657b5feedd76cfb4ca5ac58bcd88e89b9f
cereia <104402370+cereia@users.noreply.github.com>
2024-04-25 17:29:51 -0400

    Refactor code to privatize some methods

--- script.rb
+++ script.rb
@@ -3,36 +3,54 @@
 # create a tic tac toe game where two humans can play
 # the board is meant to be displayed in the console after every player turn
 class Board
-  attr_reader :board, :round, :player1_positions, :player2_positions
+  attr_reader :board, :round, :player1, :player2, :player1_positions, :player2_positions
 
-  def initialize(hash)
+  def initialize
     @board = []
-    0.upto(8) { |i| @board[i] = '' }
+    0.upto(8) { |i| board[i] = '' }
     @round = 0
-    @player1 = hash[:player1]
-    @player2 = hash[:player2]
     @player1_positions = []
     @player2_positions = []
-    puts "Player 1: #{@player1}; Player 2: #{@player2}"
+    assign_symbol
     puts "Board created: #{board}"
   end
 
   def place(position)
     if board[position - 1] == ''
-      @round += 1
-      mark = round.odd? ? @player1 : @player2
+      inc_round
+      mark = which_mark
       board[position - 1] = mark
       position_recorder(mark, position)
     else
       puts "There's already an #{board[position - 1]} at #{position}. Please choose again."
     end
-    puts "Round: #{@round} #{board}"
+    puts "Round: #{round} #{board}"
+  end
+
+  def which_mark
+    round.odd? ? player1 : player2
+  end
+
+  private
+
+  def assign_symbol
+    puts 'Do you want to be X or O?'
+    mark = gets.chomp
+    if mark.match(/x|o/i) && mark.length == 1
+      @player1 = mark.match(/x|o/i).to_s.upcase
+      @player2 = player1 == 'X' ? 'O' : 'X'
+      puts "Player 1: #{player1}; Player 2: #{player2}"
+    else
+      assign_symbol
+    end
+  end
+
+  def inc_round
+    @round += 1
   end
 
   def position_recorder(symbol, position)
-    symbol == @player1 ? player1_positions.push(position) : player2_positions.push(position)
-    # puts "p1 positions #{player1_positions}"
-    # puts "p2 positions #{player2_positions}"
+    symbol == player1 ? player1_positions.push(position) : player2_positions.push(position)
   end
 end
 
@@ -41,23 +59,10 @@ def play_game
   answer = gets.chomp
   play_game unless answer[0].match(/y|n/i)
   if answer[0].match(/y/i)
-    board = Board.new(assign_symbol)
+    board = Board.new
     play_round(board)
   elsif answer[0].match(/n/i)
     puts ':( No tic tac toe'
-  end
-end
-
-# move this code to Board? as part of setter?
-def assign_symbol
-  puts 'Do you want to be X or O?'
-  mark = gets.chomp
-  if mark.match(/x|o/i) && mark.length == 1
-    player1 = mark.match(/x|o/i).to_s.upcase
-    player2 = player1 == 'X' ? 'O' : 'X'
-    { player1: player1, player2: player2 }
-  else
-    assign_symbol
   end
 end
 
@@ -78,7 +83,7 @@ def restart
   puts 'Would you like to play again? Y/N'
   answer = gets.chomp
   if answer[0].match(/y/i)
-    board = Board.new(assign_symbol)
+    board = Board.new
     play_round(board)
   else
     puts 'Thank you for playing :)'
