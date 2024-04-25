c593415a7e58a77af05b903350a2d37f203bd359
cereia <104402370+cereia@users.noreply.github.com>
2024-04-25 16:36:06 -0400

    Add conditions to ensure input is 1 char/digit
    Fixed some issues caused by recursive calls being outside conditions

--- script.rb
+++ script.rb
@@ -24,7 +24,7 @@ class Board
       board[position - 1] = mark
       position_recorder(mark, position)
     else
-      puts 'There\'s already a symbol there. Please choose again.'
+      puts "There's already an #{board[position - 1]} at #{position}. Please choose again."
     end
     puts "Round: #{@round} #{board}"
   end
@@ -39,13 +39,12 @@ end
 def play_game
   puts 'Would you like to play tic tac toe? Y/N'
   answer = gets.chomp
+  play_game unless answer[0].match(/y|n/i)
   if answer[0].match(/y/i)
     board = Board.new(assign_symbol)
     play_round(board)
   elsif answer[0].match(/n/i)
     puts ':( No tic tac toe'
-  else
-    play_game
   end
 end
 
@@ -53,8 +52,8 @@ end
 def assign_symbol
   puts 'Do you want to be X or O?'
   mark = gets.chomp
-  if mark.match(/x/i) || mark.match(/o/i)
-    player1 = (mark.match(/x/i) || mark.match(/o/i)).to_s.upcase
+  if mark.match(/x|o/i) && mark.length == 1
+    player1 = mark.match(/x|o/i).to_s.upcase
     player2 = player1 == 'X' ? 'O' : 'X'
     { player1: player1, player2: player2 }
   else
@@ -63,17 +62,16 @@ def assign_symbol
 end
 
 def play_round(game_board)
-  puts 'Please choose a number from 1 to 9'
+  puts 'Please choose a number from 1 to 9.'
   num = gets.chomp
-  # currently not matching properly, goes to the else condition of board.place when number > 9
-  if num.match(/[1-9]/)
+  if num.match(/[1-9]/) && num.length == 1
     game_board.place(num.to_s.to_i)
+    game_board.round < 9 ? play_round(game_board) : restart
   else
     play_round(game_board)
   end
   # puts "player 1 #{game_board.player1_positions}"
   # puts "player 2 #{game_board.player2_positions}"
-  game_board.round < 9 ? play_round(game_board) : restart
 end
 
 def restart
